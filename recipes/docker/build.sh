1d9ad9b51b7b7cbf6684fe149873d051ebbd9e7a
Dennis Maisenbacher <dennis.maisenbacher@wdc.com>
2024-09-03 13:31:32 +0000

    feat: Improve container image build time
    Suggested-by: https://github.com/jmhands
    Signed-off-by: Dennis Maisenbacher <dennis.maisenbacher@wdc.com>

--- recipes/docker/build.sh
+++ recipes/docker/build.sh
@@ -2,9 +2,9 @@
 
 # to prune all images, execute "podman prune -a". Note that it will remove ALL! images loaded.
 
-podman build -t zfio fio
-podman build -t zspdk-fio spdk/uring
-podman build -t zrocksdb rocksdb
-podman build -t zf2fs f2fs
-podman build -t zxfs xfs
-podman build -t zsysbench sysbench
+podman build --jobs=$(nproc) -t zfio fio
+podman build --jobs=$(nproc) -t zspdk-fio spdk/uring
+podman build --jobs=$(nproc) -t zrocksdb rocksdb
+podman build --jobs=$(nproc) -t zf2fs f2fs
+podman build --jobs=$(nproc) -t zxfs xfs
+podman build --jobs=$(nproc) -t zsysbench sysbench
